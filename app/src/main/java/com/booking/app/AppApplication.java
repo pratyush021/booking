a1be638a355d42b63c81d8e79b10191a41322b83
Pratyush Ranjan Bohidar <53327828+pratyush021@users.noreply.github.com>
2025-08-30 11:18:21 +0530

    first commit

---
+++ app/src/main/java/com/booking/app/AppApplication.java
@@ -0,0 +1,13 @@
+package com.booking.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
